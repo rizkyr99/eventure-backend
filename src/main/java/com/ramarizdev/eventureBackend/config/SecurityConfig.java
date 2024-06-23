9b7e86d92e74e294e9af6ede9c78692aa5be9553
Rizky Ramadhan <ramarizdev@gmail.com>
2024-06-23 16:08:28 +0700

    feat(security): add bean annotation to filterChain

--- src/main/java/com/ramarizdev/eventureBackend/config/SecurityConfig.java
+++ src/main/java/com/ramarizdev/eventureBackend/config/SecurityConfig.java
@@ -1,9 +1,15 @@
 package com.ramarizdev.eventureBackend.config;
 
 import lombok.extern.java.Log;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 @Configuration
 @EnableWebSecurity
@@ -14,5 +20,18 @@ public class SecurityConfig {
 
     public SecurityConfig(RsaKeyConfigProperties rsaKeyConfigProperties) {
         this.rsaKeyConfigProperties = rsaKeyConfigProperties;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http, HandlerMappingIntrospector introspector) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/api/v1/events").permitAll();
+                    auth.anyRequest().authenticated();
+                })
+                .httpBasic(Customizer.withDefaults())
+                .build();
     }
 }
