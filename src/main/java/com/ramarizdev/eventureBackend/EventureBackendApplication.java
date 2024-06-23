967e0612c3d7f243960f06fbdfc61afa3cbc810f
Rizky Ramadhan <ramarizdev@gmail.com>
2024-06-23 14:31:39 +0700

    Initial commit

---
+++ src/main/java/com/ramarizdev/eventureBackend/EventureBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ramarizdev.eventureBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventureBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventureBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ramarizdev/eventureBackend/EventureBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramarizdev.eventureBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventureBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
